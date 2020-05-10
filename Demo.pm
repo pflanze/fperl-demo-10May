addb54022e845d1e465272e0630bb3afc4fbd738
Christian Jaeger (Rustaceans) <ch@christianjaeger.ch>
2020-05-10 20:53:28 +0100

    start

---
+++ Demo.pm
@@ -0,0 +1,10 @@
+package Demo;
+@ISA="Exporter"; require Exporter;
+@EXPORT=qw();
+@EXPORT_OK=qw();
+%EXPORT_TAGS=(all=>[@EXPORT,@EXPORT_OK]);
+
+use strict; use warnings; use warnings FATAL => 'uninitialized';
+
+
+1
