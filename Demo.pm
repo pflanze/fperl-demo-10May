f95609345aa9976a53b06ef03e1442472398e89b
Christian Jaeger (Rustaceans) <ch@christianjaeger.ch>
2020-05-10 22:19:45 +0100

    (commit)
    M 2020-05-10 21:46 demo
    M 2020-05-10 21:48 Demo.pm

--- Demo.pm
+++ Demo.pm
@@ -1,10 +1,22 @@
 package Demo;
 @ISA="Exporter"; require Exporter;
-@EXPORT=qw();
+@EXPORT=qw(foo);
 @EXPORT_OK=qw();
 %EXPORT_TAGS=(all=>[@EXPORT,@EXPORT_OK]);
 
 use strict; use warnings; use warnings FATAL => 'uninitialized';
 
+use FP::Repl;
+
+
+sub foo {
+    my ($x)=@_;
+    if ($x < 100) {
+	foo( $x+1 )
+    } else {
+	repl;
+	#die "hi"
+    }
+}
 
 1
