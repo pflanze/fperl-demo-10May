aa5223cf16d7da1f29a111e70720ade547325c32
Christian Jaeger (Rustaceans) <ch@christianjaeger.ch>
2020-05-10 22:46:17 +0100

    Add `mystream`, simpler version of `stream`

--- Demo.pm
+++ Demo.pm
@@ -1,13 +1,33 @@
 package Demo;
 @ISA="Exporter"; require Exporter;
-@EXPORT=qw(foo);
+@EXPORT=qw(mystream foo);
 @EXPORT_OK=qw();
 %EXPORT_TAGS=(all=>[@EXPORT,@EXPORT_OK]);
 
 use strict; use warnings; use warnings FATAL => 'uninitialized';
 
-use FP::Repl;
+use FP::List qw(cons null);
+use FP::Lazy 'lazy';
+use FP::Stream; # imports `Weakened`, and also to load the lazy sequence
+                # operators so that
+                # e.g. `mystream(5,6,7)->map(sub{$_[0]*2})` calculates
+                # its result lazily, too.
 
+sub mystream {
+    my @elements= @_;
+    my $next; $next= sub {
+	my ($i)= @_;
+	my $next= $next; # I'd love for this not to be necessary
+	lazy {
+	    $i <= $#elements
+	      ? cons( $elements[$i], &$next($i+1) )
+	      : null # (null is the end of list marker, same as `list()`)
+	}
+    };
+    Weakened($next)->(0)  # I'd love for the `Weakened` not to be necessary
+}
+
+use FP::Repl;
 
 sub foo {
     my ($x)=@_;
